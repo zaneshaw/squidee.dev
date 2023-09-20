ba086fc2926970ebb94b1d881fabea13d64c2942
Zane Shaw <admin@triangle100.com>
2023-09-21 09:25:54 +1000

    Setup for online/offline status

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,33 @@
 <script lang="ts">
 	import "../app.postcss";
 	import NavButton from "$lib/components/NavButton.svelte";
+	import { fade } from "svelte/transition";
+
+	let statusPopover = false;
+	let status = true;
 </script>
 
 <div class="mx-auto flex min-h-screen w-[750px] flex-col gap-2 py-5">
 	<div>
-		<h1 class="text-center font-medium">squidee.dev</h1>
+		<div class="relative mx-auto w-fit">
+			<h1 class="text-center font-medium">squidee.dev</h1>
+			<div
+				on:mouseenter={() => (statusPopover = true)}
+				on:mouseleave={() => (statusPopover = false)}
+				aria-hidden
+				class="absolute -right-5 top-1/2 w-3 -translate-y-1/2 rounded-full {status ? "bg-green-500" : "bg-red-500"}"
+			>
+				{#if statusPopover}
+					<div
+						transition:fade={{ duration: 75 }}
+						class="absolute -right-3 top-1/2 -translate-y-1/2 translate-x-full w-max rounded bg-white px-3 py-1 shadow"
+					>
+						I am {status ? "ONLINE" : "OFFLINE"}
+					</div>
+				{/if}
+				<div class="aspect-square animate-ping rounded-full {status ? "bg-green-500" : "bg-red-500"}"></div>
+			</div>
+		</div>
 		<nav class="flex justify-center gap-4 py-2 text-xl">
 			<NavButton path="/" text="Home" />
 			<span class="font-semibold">·</span>
