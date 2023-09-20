e0161acece8899f87f260b1e1edf481ba558b844
Zane Shaw <admin@triangle100.com>
2023-09-20 20:20:34 +1000

    Change root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,19 +5,19 @@
 	import NavButton from "$lib/components/NavButton.svelte";
 </script>
 
-<div class="mx-auto flex min-h-screen w-[750px] flex-col bg-white p-10 pt-5">
+<div class="mx-auto flex min-h-screen w-[750px] flex-col py-5 gap-2">
 	<div>
-		<h1 class="text-center">squidee.dev</h1>
+		<h1 class="text-center font-medium">squidee.dev</h1>
 		<nav class="flex justify-center gap-4 py-2">
 			<NavButton path="/" text="Home" />
 			<span>·</span>
 			<NavButton path="/squidcraft" text="SquidCraft M" />
 		</nav>
 	</div>
-	<div class="grow">
+	<div class="grow bg-white p-10 rounded-2xl">
 		<slot />
 	</div>
-	<div class="mt-10 flex justify-between">
+	<div class="px-10 flex justify-between">
 		<span>© 2023 Zane Shaw. All rights reserved.</span>
 		<a href="/">squidee.dev</a>
 	</div>
