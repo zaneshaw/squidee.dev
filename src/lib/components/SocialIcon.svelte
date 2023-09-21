5b5ffd57046d604456c19f9ec22e6f09839e2cfe
Zane Shaw <admin@triangle100.com>
2023-09-21 20:29:45 +1000

    Add social icons to footer

---
+++ src/lib/components/SocialIcon.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let href: string;
+	export let title: string;
+	export let style: string;
+</script>
+
+<a {href} {title} target="_blank" class="w-5 cursor-pointer fill-violet-300 transition-colors {style}">
+	<slot />
+</a>
