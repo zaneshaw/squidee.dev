99861f839a836349e4160c167533c2452920e844
Zane Shaw <admin@triangle100.com>
2023-09-23 18:02:04 +1000

    Remove join application receipt

--- src/routes/squidcraft/join/success/+page.svelte
+++ src/routes/squidcraft/join/success/+page.svelte
@@ -1,11 +1,3 @@
-<script lang="ts">
-	import { browser } from "$app/environment";
-	import { page } from "$app/stores";
-
-	let res: any[];
-	$: if (browser) res = Array.from($page?.url?.searchParams || []);
-</script>
-
 <svelte:head>
 	<title>squidee.dev - Application submitted</title>
 </svelte:head>
@@ -13,7 +5,5 @@
 <div class="mx-auto flex flex-col rounded-lg px-10 py-8 ring-2 ring-violet-300">
 	<h1>Application submitted!</h1>
 	<p class="mb-4">Zane will review your submission soon and add you to the whitelist :D</p>
-	<code>Name: {res?.[0][1]}</code>
-	<code>IGN: {res?.[1][1]}</code>
 	<a href="/squidcraft" class="link ml-auto">← Done</a>
 </div>
