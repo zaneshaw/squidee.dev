09d5954711031a3b3dd2e81a5b412bff5d5273bc
Zane Shaw <zane@squidee.dev>
2024-08-03 18:56:42 +1000

    Add blog list

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,1 +1,22 @@
 <h1>blog</h1>
+<div class="flex flex-col gap-5">
+	<label class="flex">
+		<img src="search.svg" alt="search icon" class="cursor-text border-b border-white pr-1" />
+		<input type="text" placeholder="search posts" class="input h-12 w-full" />
+	</label>
+	<div class="flex flex-col items-center">
+		{#each { length: 6 } as _}
+			<div class="group relative flex w-full cursor-pointer flex-col py-4 transition-all hover:pb-8">
+				<div class="flex justify-between gap-5">
+					<div>
+						<h1 class="relative font-normal text-neutral-300 transition-colors group-hover:text-white">this is a blog post</h1>
+						<p class="line-clamp-2 break-all text-neutral-400 transition-colors group-hover:text-neutral-300">this a descriptive description</p>
+					</div>
+					<h2 class="flex-shrink-0 font-light text-neutral-300 transition-colors group-hover:text-white">3 aug</h2>
+				</div>
+
+				<span class="absolute bottom-1 font-semibold text-neutral-300 opacity-0 transition-all group-hover:opacity-100">read &rarr;</span>
+			</div>
+		{/each}
+	</div>
+</div>
