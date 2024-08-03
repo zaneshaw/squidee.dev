a456c88c079d83f564763b7e63c0c815ce062a9b
Zane Shaw <zane@squidee.dev>
2024-08-04 01:41:41 +1000

    Add page transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,21 @@
 <script lang="ts">
-	import Terminal from "$lib/components/Terminal.svelte";
 	import "../app.postcss";
+	import { fly } from "svelte/transition";
+	import { navigating } from "$app/stores";
+	import Terminal from "$lib/components/Terminal.svelte";
+
+	export let data;
+
+	let direction: number;
+	let routes = ["/", "/folio", "/blog"];
+	$: if ($navigating) changeDir($navigating);
+
+	const changeDir = (nav: any) => {
+		const from = routes.indexOf(nav.from?.route.id);
+		const to = routes.indexOf(nav.to?.route.id);
+
+		direction = from > to ? -1 : 1;
+	}
 </script>
 
 <header class="flex gap-5 py-8">
@@ -8,8 +23,10 @@
 	<a href="/folio" class="link pr-4">folio</a>
 	<a href="/blog" class="link pr-4">blog</a>
 </header>
-<main>
-	<slot />
-</main>
+{#key data.pathname}
+	<main in:fly={{ x: 10 * direction, duration: 200, delay: 200 }} out:fly={{ x: -10 * direction, duration: 200 }}>
+		<slot />
+	</main>
+{/key}
 
 <Terminal />
---
+++ src/routes/+layout.ts
@@ -0,0 +1,7 @@
+import type { LayoutLoad } from "./$types";
+
+export const load: LayoutLoad = ({ url }) => {
+	const { pathname } = url;
+
+	return { pathname };
+};
