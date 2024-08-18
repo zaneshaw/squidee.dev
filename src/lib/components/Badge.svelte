5f121256fd3394c55cf8e9e76ab62db091106157
Zane Shaw <zane@squidee.dev>
2024-08-19 00:49:22 +1000

    Tweak tech stack icons

--- src/lib/components/Badge.svelte
+++ src/lib/components/Badge.svelte
@@ -4,11 +4,11 @@
 </script>
 
 <div
-	class="group/tag flex h-6 cursor-pointer gap-0.5 rounded {color == 'red'
+	class="group/tag flex h-6 cursor-pointer gap-0.5 rounded px-1.5 transition-colors {color == 'red'
 		? 'bg-red-500/[0.15]'
 		: color == 'yellow'
 		? 'bg-yellow-500/[0.15]'
-		: 'bg-green-500/[0.15]'} px-1 transition-colors"
+		: 'bg-green-500/[0.15]'}"
 >
-	<span class="my-auto text-sm font-light leading-none tracking-tighter text-neutral-400 transition-colors group-hover/tag:text-white">{label}</span>
+	<span class="my-auto text-sm font-light leading-none tracking-[-0.075em] text-neutral-400 transition-colors group-hover/tag:text-white">{label}</span>
 </div>
