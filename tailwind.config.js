ee10504d5e2012fadc36f4a7236f1e94b88d2862
Zane Shaw <zane@squidee.dev>
2025-08-26 20:57:14 +1000

    Use official Inter CDN

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,6 @@ export default {
 			animation: {
 				"ping-slow": "ping 1.5s cubic-bezier(0, 0, 0.2, 1) infinite"
 			},
-			fontFamily: {
-				sans: ["Inter", "Arial", "sans-serif"]
-			}
 		}
 	},
 	plugins: []
