cf130b3cf8b0aaca5778e842904a8ca5c9ec519f
Zane Shaw <zane@squidee.dev>
2025-09-03 14:53:58 +1000

    Add backend admin auth

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,4 +1,6 @@
 import PocketBase from "pocketbase";
+import { env } from "$env/dynamic/private";
 
-export const pb = new PocketBase("https://backend.squidee.dev");
+export const pb = new PocketBase(env.BACKEND_URL);
 
+pb.collection("_superusers").authWithPassword(env.BACKEND_EMAIL, env.BACKEND_PASSWORD);
