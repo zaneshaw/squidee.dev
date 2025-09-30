585f6223b27bbb6ad03069a0a505fda3a8215b64
Zane Shaw <zane@squidee.dev>
2025-09-30 15:46:54 +1000

    Prevent expired auth token (hopefully)

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,5 +3,7 @@ import { env } from "$env/dynamic/private";
 import type { ServerInit } from "@sveltejs/kit";
 
 export const init: ServerInit = async () => {
-	await pb.collection("_superusers").authWithPassword(env.BACKEND_EMAIL, env.BACKEND_PASSWORD);
+	await pb.collection("_superusers").authWithPassword(env.BACKEND_EMAIL, env.BACKEND_PASSWORD, {
+		autoRefreshThreshold: 30 * 60
+	});
 };
